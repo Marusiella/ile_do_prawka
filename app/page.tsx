8c2aa0584ff917d4b1b75fcc336285fc14dd9bcc
Marusiella <67511608+Marusiella@users.noreply.github.com>
2024-06-28 17:51:50 +0200

    klauzula

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,11 @@ import Main from "./Main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Main />
+      <div
+      className="text-center text-sm text-gray-500"
+      >Nie biorę odpowiedzialności za błędy w obliczeniach. Wszelkie dane są przybliżone.</div>
     </main>
   );
 }
